26d5a25e49d987657c077fd7fb82f8bfeb5f1a75
cysun <chiayusun.cs08g@nctu.edu.tw>
2021-11-10 23:05:44 +0800

    ch10(sort_array): add quickSort (TLE for sorted array)

--- ch10-sort_and_search/sort_array.cpp
+++ ch10-sort_and_search/sort_array.cpp
@@ -10,6 +10,39 @@ using namespace std;
 
 class Solution {
 public:
+    void swap(vector<int>& nums, int front, int end) {
+        int tmp = nums[front];
+        nums[front] = nums[end];
+        nums[end] = tmp;
+    }
+
+    int partition(vector<int>& nums, int front, int end) {
+        int pivot = nums[end];
+        int idx_front = front-1;
+        for (size_t idx_end = front; idx_end < end; idx_end++){
+            if (nums[idx_end] <= pivot) {
+                idx_front++;
+                swap(nums, idx_front, idx_end);
+            }
+        }
+        idx_front++;
+        swap(nums, idx_front, end);
+        
+        return idx_front;
+    }
+
+    void quickSort(vector<int>& nums, int front, int end) {
+        if (front < end) {
+            int pivot = partition(nums, front, end);
+            quickSort(nums, front, pivot-1);
+            quickSort(nums, pivot+1, end);
+        }
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(nums, 0, nums.size()-1);
+        return nums;
+    }
 
     vector<int> bubbleSort(vector<int>& nums) {
         for (size_t i = 0; i < nums.size()-1; i++) {
@@ -34,7 +67,8 @@ public:
 
 int main() {
     std::vector<int> input{2, 6, 5, 3, 8, 7, 1, 0};
-    Solution().bubbleSort(input);
+    // std::vector<int> input{5, 1, 1, 2, 0, 0};
+    Solution().sortArray(input);
     Solution().printArray(input);
 
     return 0;
