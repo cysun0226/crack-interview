b356cbd01b818a62469d9e58a793a91b4cdac72e
cysun <chiayusun.cs08g@nctu.edu.tw>
2021-11-12 16:21:16 +0800

    ch4(completeness): add Leetcode AC code (recursive)

--- ch4-trees_and_graphs/completeness.cpp
+++ ch4-trees_and_graphs/completeness.cpp
@@ -3,61 +3,61 @@
 * https://leetcode.com/problems/check-completeness-of-a-binary-tree/
 */
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
 public:
-    struct Checker {
-        int height;
-        int count;
-        Checker(int h, int c): height(h), count(c) {}
-    };
-    
     bool isCompleteTree(TreeNode* root) {
-        return (check(root).height == -1)? false : true;
+        if (root == nullptr)
+            return true;
+        
+        int node_count = count(root);
+        
+        return check(root, 0, node_count);
     }
     
-    Checker check(TreeNode* root) {
-        // skip the left node
-        if (root->left == nullptr && root->right != nullptr)
-            return Checker(-1, -1);
-        
-        // leaf
-        if (root->left == nullptr && root->right == nullptr)
-            return Checker(1, 1);
-        
-        int r_height, l_height, height, count;
-        Checker l_check = check(root->left);
-        Checker r_check = (root->right == nullptr)? Checker(0, 0) : check(root->right);
-        
-        if (l_check.height == -1 || r_check.height == -1)
-            return Checker(-1, -1);
-        
-        // height diff > 1
-        if (abs(l_check.height - r_check.height) > 1)
-            return Checker(-1, -1);
-        
-        // #right > #left
-        if (l_check.count < r_check.count)
-            return Checker(-1, -1);
-        
-        // left is not full, then right should not have any node
-        if (l_check.height == r_check.height && l_check.count % 2 == 0)
-            return Checker(-1, -1);
-        
-        // right is not full, but the left keep growing
-        if (r_check.count % 2 == 0 && r_check.count > ) {
-            return Checker(-1, -1);
-        }   
-        
-        height = (l_check.height > r_check.height)? l_check.height : r_check.height;
-        height += 1;
-        int r_count, l_count;
-        r_count = (root->right == nullptr)? 0: r_check.count;
-        l_count = (root->left == nullptr)? 0: l_check.count;
-        
-        count = l_count + r_count + 1;
-        
-        return Checker(height, count);
+    int count(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        return count(root->left) + count(root->right) + 1;
     }
     
-    
+    bool check(TreeNode* root, int index, int count) {
+        if (root == nullptr)
+            return true;
+        if (index >= count)
+            return false;
+        
+        
+        return (check(root->left, 2*index+1, count) && check(root->right, 2*index+2, count));
+    }
 };
+
+int main() {
+    vector<TreeNode> nodes;
+    for (size_t i = 0; i < 6; i++){
+        nodes.push_back(TreeNode());
+    }
+
+    nodes[0].left = &nodes[1];
+    nodes[0].right = &nodes[2];
+    nodes[1].left = &nodes[3];
+    nodes[1].right = &nodes[4];
+    nodes[2].left = &nodes[5];
+
+    Solution().isCompleteTree(&nodes[0]);
+
+    return 0;
+}
