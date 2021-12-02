e3164896886f024a1f7e2c3ad0eeffb015b7f7e7
cysun <chiayusun.cs08g@nctu.edu.tw>
2021-12-02 18:32:05 +0800

    ch2(intersection): add the O(n)*3 solution

--- ch2-linked_lists/intersection.cpp
+++ ch2-linked_lists/intersection.cpp
@@ -11,7 +11,7 @@
 
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    ListNode *getIntersectionNode_bf(ListNode *headA, ListNode *headB) {
         ListNode *ptrA = headA, *ptrB, *intersect = nullptr;
         while (ptrA != nullptr && intersect == nullptr) {
             ptrB = headB;
@@ -27,4 +27,44 @@ public:
 
         return intersect;
     }
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *ptrA = headA, *ptrB = headB, *intersect = nullptr;
+        
+        // count the length of two lists
+        int lenA = 0, lenB = 0;
+        while (ptrA != nullptr) lenA++;
+        while (ptrB != nullptr) lenB++;
+
+        ptrA = headA, ptrB = headB;
+
+        // skip the non-intersect nodes
+        if (lenA >= lenB) {
+            int skip = lenA - lenB;
+            while (skip > 0) {
+                ptrA = ptrA->next;
+                skip--;
+            }
+        }
+        else {
+            int skip = lenB - lenA;
+            while (skip > 0) {
+                ptrB = ptrB->next;
+                skip--;
+            }
+        }
+
+        // find the starting point of the intersection
+        while (ptrA != nullptr) {
+            if (ptrB == ptrA) {
+                intersect = ptrB;
+                break;
+            }
+            ptrA = ptrA->next;
+            ptrB = ptrB->next;
+        }
+
+        return intersect;
+    }
+
 };
