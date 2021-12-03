9d5c69a14342d0c244814424b53f621ce953254d
cysun <chiayusun.cs08g@nctu.edu.tw>
2021-12-04 00:21:12 +0800

    ch10(top_k_frequent): add the priority queue solution

--- ch10-sort_and_search/top_k_frequent.cpp
+++ ch10-sort_and_search/top_k_frequent.cpp
@@ -6,14 +6,41 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <queue>
+
 using namespace std;
 
 class Solution {
-// private:
-    // typedef map<int, int> F_MAP;
-
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int, int> frequency;
+
+        // count the frequencies
+        for (int i : nums) {
+            if (frequency.find(i) == frequency.end())
+                frequency[i] = 1;
+            else
+                frequency[i]++;
+        }
+
+        // sort by the counted value
+        priority_queue<pair<int, int>> sort_freq;
+        for (auto &[key, value] : frequency) {
+            sort_freq.push({value, key});
+        }
+
+        // output the result
+        vector<int> top_k;
+        while (top_k.size() != k) {
+            auto top = sort_freq.top();
+            sort_freq.pop();
+            top_k.push_back(top.second);
+        }
+
+        return top_k;
+    }
+
+    vector<int> topKFrequent_map(vector<int>& nums, int k) {
         map<int, int> frequency;
 
         // count the frequencies
