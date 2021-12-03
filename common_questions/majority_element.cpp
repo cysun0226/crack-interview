a560290b6795505ccd4df6dda3a3862815fb6c80
cysun <chiayusun.cs08g@nctu.edu.tw>
2021-12-04 01:07:32 +0800

    common(majority_element): add the Boyer-Moore majority vote solution

--- common_questions/majority_element.cpp
+++ common_questions/majority_element.cpp
@@ -24,4 +24,18 @@ public:
         
         return sort_count[0].first;
     }
+
+    int moore_vote(vector<int>& nums) {
+        int major = nums[0], count = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (count == 0) {
+                major = nums[i];
+                count++;
+            } 
+            else if (major == nums[i]) count++;
+            else count--;
+        }
+
+        return major;
+    }
 };
