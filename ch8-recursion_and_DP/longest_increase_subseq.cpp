04775e03a0afa4d25fd5be4d56ba05bfacf78005
cysun <chiayusun.cs08g@nctu.edu.tw>
2021-12-05 18:53:41 +0800

    ch8(longest_asc_subseq): add the DP O(n2) solution

--- ch8-recursion_and_DP/longest_increase_subseq.cpp
+++ ch8-recursion_and_DP/longest_increase_subseq.cpp
@@ -1,32 +1,19 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int>> sequences;
-        for (size_t i = 0; i < nums.size(); i++) {
-            vector<int> increase_seq = {nums[i]};
-            for (size_t j = i+1; j < nums.size(); j++) {
-                int end = increase_seq.size()-1 ;
-                if (nums[j] > increase_seq.back()) {
-                    increase_seq.push_back(nums[j]);
-                }
-                else if (nums[j] > increase_seq[end-1]) {
-                    increase_seq.pop_back();
-                    increase_seq.push_back(nums[j]);
+        vector<int> curr_max(nums.size(), 1);
+        for (size_t i = 1; i < nums.size(); i++) {
+            for (size_t j = 0; j < i; j++) {
+                if (nums[j] < nums[i] && curr_max[i] < curr_max[j]+1) {
+                    curr_max[i] = curr_max[j]+1;
                 }
             }
-            sequences.push_back(increase_seq);
         }
 
-        // find the max length
-        int max_length = 0;
-        for (auto seq : sequences) {
-            if (seq.size() > max_length)
-                max_length = seq.size();
-        }
-
-        return max_length;
+        return *max_element(curr_max.begin(), curr_max.end());
     }
 };
